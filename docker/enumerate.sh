2814925248f112d0524afaba05e95429b6348c2a
Márton Kónya <konyamarci52@proton.me>
2022-11-02 21:35:46 +0100

    DevOps Sub-Bug #1 - fixed enumerate.sh

--- docker/enumerate.sh
+++ docker/enumerate.sh
@@ -1,10 +1,5 @@
-# start server in background and wait for 1 sec
-redis-server --daemonize yes && sleep 1 
-# slurp all data from file to redis in memory db (note the dir)
-redis-cli < /root/enumerate.redis 
-# persist data to disk
-redis-cli save 
-# stop background server
-redis-cli shutdown 
-# start the server normally
-redis-server 
+redis-server --daemonize yes && sleep 1; 
+redis-cli < /root/enumerate.redis ;
+redis-cli save;
+redis-cli shutdown; 
+redis-server /etc/redis/redis.conf;

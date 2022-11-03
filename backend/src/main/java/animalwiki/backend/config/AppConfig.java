5f27581b8624b7cfeccddf030e628ba124d6f93d
Kun Patrik <kunpatrik00@mailbox.unideb.hu>
2022-11-03 19:57:32 +0100

    Backend Sub-Task #11 - redis config

--- backend/src/main/java/animalwiki/backend/config/AppConfig.java
+++ backend/src/main/java/animalwiki/backend/config/AppConfig.java
@@ -1,6 +1,8 @@
 package animalwiki.backend.config;
 
+import animalwiki.backend.model.RedisConfig;
 import animalwiki.backend.repository.RedisRepository;
+import animalwiki.backend.util.json.ObjectFactory;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,14 +10,22 @@ import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 
+import java.io.IOException;
+
 @Configuration
 @EnableCaching
 public class AppConfig {
 
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("localhost", 6379);
-        redisStandaloneConfiguration.setPassword(RedisPassword.of("password"));
+        RedisConfig redisConfig;
+        try {
+            redisConfig = ObjectFactory.produce(AppConfig.class.getResourceAsStream("redis.json"), RedisConfig.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(redisConfig.getHost(), redisConfig.getPort());
+        redisStandaloneConfiguration.setPassword(RedisPassword.of(redisConfig.getPassword()));
         return new JedisConnectionFactory(redisStandaloneConfiguration);
     }
 

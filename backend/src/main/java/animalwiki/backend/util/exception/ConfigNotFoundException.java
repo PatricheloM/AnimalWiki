b36c2206a4a5f161564ec2b8712772eb89160402
Kun Patrik <kunpatrik00@mailbox.unideb.hu>
2022-11-27 17:42:42 +0100

    Backend Sub-Task #14 - changed single conn to pool

---
+++ backend/src/main/java/animalwiki/backend/util/exception/ConfigNotFoundException.java
@@ -0,0 +1,16 @@
+package animalwiki.backend.util.exception;
+
+public class ConfigNotFoundException extends RuntimeException {
+
+    public ConfigNotFoundException(String msg) {
+        super(msg);
+    }
+
+    public ConfigNotFoundException(Exception e) {
+        super(e);
+    }
+
+    public ConfigNotFoundException() {
+        super();
+    }
+}

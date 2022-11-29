df0ba8d42f478045ba28c103ced6b3d9b2486ca1
Vangel Attila <vangelll123@gmail.com>
2022-11-30 00:43:01 +0100

    User Sub-Task #3 - populate the searchbar

--- frontend/src/Components/SearchBar.js
+++ frontend/src/Components/SearchBar.js
@@ -1,6 +1,6 @@
 import React from "react";
-// import { useEffect, useState } from "react";
-// import axios from "axios";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function SearchBar({ placeholder }) {
   // const [anim, setAnim] = useState([]);
@@ -11,11 +11,35 @@ function SearchBar({ placeholder }) {
   //   }
   //   getAnimals();
   // }, []);
+  const [value, setValue] = useState([]);
+  const [res, setRes] = useState([]);
+  useEffect(() => {
+    async function getStoreData() {
+      const response = await axios.get(
+        `http://localhost:8080/api/query/${value}`
+      );
+      setRes(response.data);
+    }
+    getStoreData();
+  }, [value]);
+
+  let final = [];
+  res.forEach((element) => final.push(<li>{element}</li>));
+  if (!res) {
+    console.log("Array is empty!");
+    console.log("inside res" + res);
+    final = [];
+  }
+  console.log(final);
   return (
     <div className="search">
       <div className="searchInputs">
-        <input type="text" placeholder={placeholder} />
-        <div className="searchIcon"></div>
+        <input
+          type="text"
+          placeholder="hello"
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <div className="searchResult">{final}</div>
       </div>
       <div className="dataResult">
         {/* {anim.map((value) => {
